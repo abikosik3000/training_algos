12dd2f973dfd0860ce2277d539250f300beddf1c
abikosik3000 <Padla-Oleg3000@yandex.ru>
2023-06-09 13:49:00 +0300

    none

--- python/test.py
+++ python/test.py
@@ -1,11 +1,4 @@
-import threading
-import time
-def worker(num):
-    time.sleep(5 - num)
-    print(num ** 2, end='')
-t1 = threading.Thread(target=worker, args=(1,))
-t2 = threading.Thread(target=worker, args=(2,))
-t1. start()
-t2.start()
-t1.join()
-t2. join()
+x = 0
+while x < 10:
+    x = x + 1
+print(x)
